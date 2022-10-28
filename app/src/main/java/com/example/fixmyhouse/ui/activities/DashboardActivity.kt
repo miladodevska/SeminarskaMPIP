81f3224fe472856a119b7c538164ed0753fb948b
Mila Dodevska <dodevskamila@gmail.com>
2022-10-28 11:20:56 +0200

    initial commit

---
+++ app/src/main/java/com/example/fixmyhouse/ui/activities/DashboardActivity.kt
@@ -0,0 +1,47 @@
+package com.example.fixmyhouse.ui.activities
+
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.fixmyhouse.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class DashboardActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        // Update the background color of the action bar as per our design requirement.
+        supportActionBar!!.setBackgroundDrawable(
+            ContextCompat.getDrawable(
+                this@DashboardActivity,
+                R.drawable.app_gradient_color_background
+            )
+        )
+        // END
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_products,
+                R.id.navigation_dashboard,
+                R.id.navigation_orders,
+                R.id.navigation_sold_products
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onBackPressed() {
+        doubleBackToExit()
+    }
+}
